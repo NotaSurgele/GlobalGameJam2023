d1cd8289a1373b762d3d78ab43aec5844a35204c
NotaSurgele <alexis.picard@epitech.eu>
2023-02-04 09:49:49 +0100

    added playercontroller

---
+++ Assets/Scenes/Player/PlayerController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 _direction = Vector3.zero;
+
+    public float speed = 5;
+
+    public float rotationSpeed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = -Input.GetAxis("Vertical");
+        float mx = Input.GetAxis("Mouse X");
+        float my = Input.GetAxis("Mouse Y");
+        this._direction.x = x;
+        
+        this.transform.Rotate(new Vector3(mx * rotationSpeed, my * rotationSpeed, 0));
+        this.transform.Translate(this._direction * speed * Time.deltaTime);
+        Debug.Log(mx + ":" + my);
+    }
+}
